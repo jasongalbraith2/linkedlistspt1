0fec3847d8329f69af09087cb22bf5f12b362403
jasongalbraith2 <463557@bsd48.org>
2025-12-17 09:08:04 -0800

    Add comments to Node class methods
    Added comments for constructor, destructor, and accessor methods.

--- node.h
+++ node.h
@@ -8,11 +8,18 @@ private:
 	Node* next;
 	Student* data;
 public:
+
+	/* Constructor / Deconstructor */
 	Node(Student* student);
 	~Node();
 
+	/* Function returns private data (node* next) to point to the next node */
 	Node* getNext();
+
+	/* Function returns private data (student* data) to point to the student value saved */
 	Student* getStudent();
+
+	/* Function takes a node pointer to point to the next in the chain of nodes */
 	void setNext(Node* node);
 };
 
